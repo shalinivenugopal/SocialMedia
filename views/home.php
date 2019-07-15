840593bcfbbac35cbabab8e57836a18071b65aa7
spmohansp <spmohansp@gmail.com>
2019-07-15 18:21:20 +0530

    profile and online user updated

--- views/home.php
+++ views/home.php
@@ -1,8 +1,28 @@
-<?php include_once 'header.php'; ?>
-  
-  <h3>Right Aligned Navbar</h3>
-  <p>The .navbar-right class is used to right-align navigation bar buttons.</p>.
+<?php include_once 'header.php'; 
 
+ include_once '../model/db.php';
+  $id = $_SESSION['id'];
+  $sql="SELECT * FROM users WHERE user_status = 1";
+  $result = mysqli_query($conn,$sql);
+  ?>
+  
+  <div class="row">
+  	<div class="col-sm-9">
+
+  	</div>
+  	<div class="col-sm-3">
+  		<h2>Online Users</h2>
+			<?php 
+			if (mysqli_num_rows($result) > 0) {
+			    while($row = mysqli_fetch_assoc($result)) {
+			        echo '<b><p style="color:green;">'.$row['username'].'</p></b><br>';
+			    }
+			} else {
+			    echo "<p>No User Online</p>";
+			}?>
+  	</div>
+  </div>
+ 
 
   
 <?php include_once 'footer.php'; ?>
--- views/profile.php
+++ views/profile.php
@@ -1,44 +1,65 @@
 <?php include_once 'header.php'; 
-include_once '../model/db.php';
-$id = $_SESSION['id'];
+  include_once '../model/db.php';
+  $id = $_SESSION['id'];
   $sql="SELECT * FROM users WHERE id = $id";
   $result = mysqli_query($conn,$sql);
   $data = mysqli_fetch_array($result);
 ?>
   <h2>Profile</h2>
   <form action="../controller/ProfileController.php" method="POST" enctype="multipart/form-data">
-    <div class="form-group">
-      <label>Name</label>
-      <input type="text" class="form-control"  placeholder="Enter name" name="name">
-    </div>
-    <div class="form-group">
-      <label>Mobile</label>
-      <input type="number" class="form-control" value="<?php echo $data['mobile'] ?>" placeholder="Enter mobile number" name="mobile">
-    </div>
-    <div class="form-group">
-      <label>DOB</label>
-      <input type="date" format="DD-MM-YYYY" class="form-control" placeholder="Enter Date Of Birth" name="dob">
-    </div>
-    <div class="form-group">
-      <label>Bio Data</label>
-      <input type="text" class="form-control" placeholder="Enter Short Bio-data" name="bio">
-    </div>
-    <div class="form-group">
-      <label>Married Status</label><br>
-      <label><input type="radio" name="relationship" value="single">Single</label>&emsp;
-      <label><input type="radio" name="relationship" value="married">Married</label>    
-    </div>
-    <div class="form-group">
-      <label><input type="checkbox" name="hobby[]" value="coding">Coding</label>&emsp;
-      <label><input type="checkbox" name="hobby[]" value="blogging">Blogging</label>&emsp;
-      <label><input type="checkbox" name="hobby[]" value="music">Music</label>&emsp;
-      <label><input type="checkbox" name="hobby[]" value="dance">Dance</label>&emsp;
-      <label><input type="checkbox" name="hobby[]" value="sleeping">Sleeping</label>
-    </div><br>
-    <div class="form-group">
-      <label for="upload">Profile Image</label>
-      <input type="file" name="profile_image">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+     <div class="form-group">
+        <label>Name</label>
+        <input type="text" class="form-control"  placeholder="Enter name" name="name">
+     </div>
+     <div class="form-group">
+        <label>Mobile</label>
+        <input type="number" class="form-control" value="<?php echo $data['mobile'] ?>" placeholder="Enter mobile number" name="mobile">
+     </div>
+     <div class="form-group">
+        <label>DOB</label>
+        <input type="date" format="DD-MM-YYYY" class="form-control" placeholder="Enter Date Of Birth" value="<?php echo $data['dob'] ?>" name="dob">
+     </div>
+     <div class="form-group">
+        <label>Bio Data</label>
+        <input type="text" class="form-control" placeholder="Enter Short Bio-data" value="<?php echo $data['bio'] ?>" name="bio">
+     </div>
+     <div class="form-group">
+        <label>Married Status</label><br>
+        <label><input type="radio" name="relationship" value="single" <?php if($data['relationship'] == 'single'){echo 'checked';} ?> >Single</label>&emsp;
+        <label><input type="radio" name="relationship" value="married" <?php if($data['relationship'] == 'married'){echo 'checked';} ?> >Married</label>    
+     </div>
+     <div class="form-group">
+        <label><input type="checkbox" name="hobby[]" value="coding" <?php if (!empty(unserialize($data['hobby']))) {
+          if (in_array('coding', unserialize($data['hobby']))) {
+            echo 'checked';
+          }
+        } ?>>Coding</label>&emsp;
+        <label><input type="checkbox" name="hobby[]" value="blogging"<?php if (!empty(unserialize($data['hobby']))) {
+          if (in_array('blogging', unserialize($data['hobby']))) {
+            echo 'checked';
+          }
+        } ?>>Blogging</label>&emsp;
+        <label><input type="checkbox" name="hobby[]" value="music"<?php if (!empty(unserialize($data['hobby']))) {
+          if (in_array('music', unserialize($data['hobby']))) {
+            echo 'checked';
+          }
+        } ?>>Music</label>&emsp;
+        <label><input type="checkbox" name="hobby[]" value="dance"<?php if (!empty(unserialize($data['hobby']))) {
+          if (in_array('dance', unserialize($data['hobby']))) {
+            echo 'checked';
+          }
+        } ?>>Dance</label>&emsp;
+        <label><input type="checkbox" name="hobby[]" value="sleeping"<?php if (!empty(unserialize($data['hobby']))) {
+          if (in_array('sleeping', unserialize($data['hobby']))) {
+            echo 'checked';
+          }
+        } ?>>Sleeping</label>
+     </div>
+     <br>
+     <div class="form-group">
+        <label for="upload">Profile Image</label>
+        <input type="file" name="profile_image">
+     </div>
+     <button type="submit" class="btn btn-primary">Update</button>
   </form>
 <?php include_once 'footer.php'; ?>
